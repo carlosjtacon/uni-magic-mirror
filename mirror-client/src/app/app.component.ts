8de9ac921689884ab6f43983135af87ae050aa1b
Carlos Tacón <carlosjtacon@gmail.com>
2017-12-09 19:28:30 +0100

    clock demo

--- mirror-client/src/app/app.component.ts
+++ mirror-client/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  clock = '';
+
+  constructor() {
+    Observable.interval(1000).subscribe(x => {
+      const date = new Date();
+      this.clock = date.getHours() + ':' + date.getMinutes();
+    });
+  }
 }
