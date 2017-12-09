05e3b4426c54251ffb7f63bd3d349fb7935b47bf
Carlos Tacón <carlosjtacon@gmail.com>
2017-12-09 19:45:01 +0100

    controlador de estados dummy
    - sustituir por peticion http

--- mirror-client/src/app/app.component.ts
+++ mirror-client/src/app/app.component.ts
@@ -8,12 +8,24 @@ import { Observable } from 'rxjs/Rx';
 })
 
 export class AppComponent {
+  controller = { status: 0, data: {} };
   clock = '';
 
   constructor() {
+    Observable.interval(2000).subscribe(x => {
+      this.controller = this.getController();
+    });
+
     Observable.interval(1000).subscribe(x => {
       const date = new Date();
       this.clock = date.getHours() + ':' + date.getMinutes();
     });
+  }
+
+  getController() {
+    // TODO: https://stackoverflow.com/questions/35316583/angular2-http-at-an-interval
+    const c = { status: 0, data: {} }
+    c.status = Math.floor(Math.random() * 2); //random entre 0 y 1
+    return c;
   }
 }
