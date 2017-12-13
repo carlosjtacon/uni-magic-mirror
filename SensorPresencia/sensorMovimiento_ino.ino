4d8394efd18a8f74509c1df4047362ebb542db79
Urbano Villanueva <u.villanueva95@gail.com>
2017-12-13 01:13:43 +0100

    sensor movimiento añadido

---
+++ SensorPresencia/sensorMovimiento_ino.ino
@@ -0,0 +1,21 @@
+//Variables
+int pinPIRinput = 2;
+int pinLED = 13;
+
+void setup() {
+  pinMode(pinLED, OUTPUT); //Pin del led como salida
+  pinMode(pinPIRinput, INPUT); //Pin del sensor como entrada
+}
+
+void loop() {
+  int valorSensor = digitalRead(pinPIRinput);
+  
+  if (valorSensor == HIGH){ //Lo que hacemos cuando el sensor da señal: hay movimiento
+    digitalWrite(pinLED, HIGH);
+    delay(5000);
+    digitalWrite(pinLED, LOW);
+    //delay(1000);
+  } else { //No da señal: el sistema se para
+    digitalWrite(pinLED, LOW);
+  }
+}
