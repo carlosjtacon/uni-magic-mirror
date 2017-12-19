abca781d931fb985445bca7052ec3362772d1d77
Nicotrial <nicotrial@gmail.com>
2017-12-19 13:28:01 +0100

    agregado sensores para cepillo y agua en arduino y actualizo el script para como el del demo agregando esto sensores

--- SenorEspejo/SenorEspejo.ino
+++ SenorEspejo/SenorEspejo.ino
@@ -1,16 +1,11 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
-
 #include "DHT.h"
 #include <Ultrasonic.h>
 
 
 #define DHTPIN 2     // what digital pin we're connected to
 
-// Uncomment whatever type you're using!
 #define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
@@ -26,9 +21,17 @@
 DHT dht(DHTPIN, DHTTYPE);
 Ultrasonic ultrasonic(12, 13);
 
+#define cepilloPin 7
+#define aguaPin 8
+
+int cepilloState = 0;
+int aguaState = 0;
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  pinMode(cepilloPin, INPUT);
+  pinMode(aguaPin, INPUT);
 }
 
 void loop() {
@@ -56,25 +59,18 @@ void loop() {
   int dist;
   boolean esta = false;
 
+  //Read values from ultrasonic sensors and compute presence
   dist = ultrasonic.distanceRead();
   if (dist <= 50){
       esta = true;
     }else{
       esta = false;  
     }
-  //Serial.print("Humidity: ");
-  //Serial.print(h);
-  //Serial.print(" %\t");
-  //Serial.print("Temperature: ");
-  //Serial.print(t);
-  //Serial.print(" *C ");
-  //Serial.print(f);
-  //Serial.print(" *F\t");
-  //Serial.print("Heat index: ");
-  //Serial.print(hic);
-  //Serial.print(" *C ");
-  //Serial.print(hif);
-  //Serial.println(" *F");
+
+  //Read values from water and brush sensors
+  cepilloState = digitalRead(cepilloPin);
+  aguaState = digitalRead(aguaPin);
+  
   Serial.print(h);
   Serial.print(",");
   Serial.print(t);
--- SenorEspejo/SensorPoster.py
+++ SenorEspejo/SensorPoster.py
@@ -1,13 +1,25 @@
-import requests
+import requests, json
 import serial
 
 ser = serial.Serial('COM4', 9600)
 while True:
-	datos = str(ser.readline())
-	print(datos[+2:-3])
 	try:
+		#datos = '36.00,23.00,73.40,1'
 		datos = str(ser.readline())[+2:-3]
 		print(datos)
-		r = requests.post("http://localhost:8000/", data={'sensor': datos})
+		sensores = datos.split(',')
+		# para athena payload = { 'status': numero } y el resto igual
+		payload = {
+			'sensors': {
+				'humidity': sensores[0],
+				'temperature': sensores[1],
+				#'temperatureF': sensores[2],
+				'presence': sensores[3],
+				'pulsadorCepillo': sensores[4],
+				'pulsadorAgua':sensores[5]
+			}
+		}
+
+		requests.post("http://localhost:8000/arduino", json=json.dumps(payload))
 	except ValueError:
-		print ("Algo no funciono" + ValueError)
+		print("Algo no funciono" + ValueError)
