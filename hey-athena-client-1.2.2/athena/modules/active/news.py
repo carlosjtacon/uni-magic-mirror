52fef124881ba82a1d192263ea193a90d964902c
Carlos Tacón <carlosjtacon@gmail.com>
2018-01-10 18:36:14 +0100

    catalonia

---
+++ hey-athena-client-1.2.2/athena/modules/active/news.py
@@ -0,0 +1,31 @@
+"""
+    Finds and returns the latest bitcoin price
+
+    Usage Examples:
+        - "What is the price of bitcoin?"
+        - "How much is a bitcoin worth?"
+"""
+
+from athena.classes.module import Module
+from athena.classes.task import ActiveTask
+
+import requests
+import json
+
+
+class GetValueTask(ActiveTask):
+
+    def __init__(self):
+        super().__init__(words=['important news'])
+
+    def action(self, text):
+        payload = {'status': 5}
+        requests.post("http://localhost:8000/athena", json=json.dumps(payload))
+        self.speak("There are some important anouncements about catalonia")
+
+
+class News(Module):
+
+    def __init__(self):
+        tasks = [GetValueTask()]
+        super().__init__('news', tasks, priority=2)
